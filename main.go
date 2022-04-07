a3261b83d5924451467f71c56565eb3b2f342ac3
Demetera <hal4ed@gmail.com>
2022-04-07 12:57:32 +0200

    add http

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 const temp_file string = "/sys/class/hwmon/hwmon1/temp3_input"
 const fan_file string = "/sys/class/hwmon/hwmon1/fan1_input"
 
-func main() {
+func hp(w http.ResponseWriter, h *http.Request) {
+	log.Println(h.RemoteAddr)
+	t := ReadValue(temp_file, 1)
+	f := ReadValue(fan_file, 2)
 
+	fmt.Fprintf(w, "<p>%d<br>%d</p>", t, f)
 }
 
-//t := ReadValue(temp_file, 1)
-//f := ReadValue(fan_file, 2)
+func main() {
+	http.HandleFunc("/", hp)
+	http.ListenAndServe(":8080", nil)
+}
