9312559e5c467126808919bf316c9043542e8833
Demeter Abroncs <hal4ed@gmail.com>
2022-04-11 22:47:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-const temp_file string = "/sys/class/hwmon/hwmon1/temp3_input"
-const fan_file string = "/sys/class/hwmon/hwmon1/fan1_input"
+const temp_file string = "ex.txt"
+//const fan_file string = "/sys/class/hwmon/hwmon1/fan1_input"
+
+type Data struct {
+	Temp int
+}
 
 func hp(w http.ResponseWriter, h *http.Request) {
+	// Temper := ReadValue(temp_file, 1)
+	mData := Data{34}	
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, mData)
 	log.Println(h.RemoteAddr)
-	t := ReadValue(temp_file, 1)
-	f := ReadValue(fan_file, 2)
 
-	fmt.Fprintf(w, "<p>%d<br>%d</p>", t, f)
 }
 
 func main() {
+	log.Println("Starting server on port 8080")
 	http.HandleFunc("/", hp)
 	http.ListenAndServe(":8080", nil)
 }
