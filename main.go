dcb61932ffed346c8f89f18cf686f3387be256eb
demetera1 <hal4ed@gmail.com>
2022-04-12 18:28:09 +0000

    UPD 220412

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-const temp_file string = "ex.txt"
+const temp_file string = "ftemp.txt"
+const fan_file string = "ffan.txt"
 //const fan_file string = "/sys/class/hwmon/hwmon1/fan1_input"
 
 type Data struct {
-	Temp int
+	Temp int	`json:"temp"`
+	Fan int		`json:"fan"`
+}
+
+var hwData Data
+
+func update (w http.ResponseWriter, h *http.Request) {
+	hwData.Temp = ReadValue(temp_file, 1)
+	hwData.Fan = ReadValue(fan_file, 2)
+	out, _ := json.MarshalIndent(hwData, "", "    ")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 func hp(w http.ResponseWriter, h *http.Request) {
-	// Temper := ReadValue(temp_file, 1)
-	mData := Data{34}	
 	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, mData)
+	t.Execute(w, nil)
 	log.Println(h.RemoteAddr)
-
 }
 
 func main() {
 	log.Println("Starting server on port 8080")
 	http.HandleFunc("/", hp)
+	http.HandleFunc("/update", update)
 	http.ListenAndServe(":8080", nil)
 }
